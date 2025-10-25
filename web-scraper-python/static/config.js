2cbc5f2d4608f3afaf71766cdf528e13a6551b27
DVaughnHouse <host@centersun21.com>
2025-10-25 10:44:56 -0400

    Add auth flow and styling updates

--- web-scraper-python/static/config.js
+++ web-scraper-python/static/config.js
@@ -1,3 +1,4 @@
 window.__CONFIG__ = {
-  apiBaseUrl: 'https://perrymill-worker.mwszghdkkf.workers.dev'
+  apiBaseUrl: 'https://perrymill-worker.mwszghdkkf.workers.dev',
+  authBaseUrl: 'https://perrymill-worker.mwszghdkkf.workers.dev'
 };
